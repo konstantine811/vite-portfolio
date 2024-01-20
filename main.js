abb2010bceaae0cdae086fa719a2b383be120743
k.abramkin <k.abramkin@kernel.ua>
2024-01-20 11:55:40 +0200

    add scss and about.html

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-import "./style.css";
+import "./style.scss";
 
 document.querySelector("#app").innerHTML = /*html*/ `
-   <h1 class="text-3xl font-bold underline text-blue-400">
-      Hello world! sdf
-    </h1>
+  <h1 class="text-3xl font-bold underline text-blue-300">Hello world!</h1>
 `;
